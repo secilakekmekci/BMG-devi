d0f71d349f8e499ab3b3ca4ef890fd12edfde673
secilakekmekci <34676724+secilakekmekci@users.noreply.github.com>
2017-12-19 12:16:56 +0300

    Add files via upload

---
+++ ConsoleApplication5/ConsoleApplication5/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            string ad; int s, k;
+            Console.WriteLine("Adınızı giriniz:"); ad = Console.ReadLine();
+            s = ad.Length;
+            for (int i = 0; i < s; i++)
+            {
+                k = (int)ad[i] + 10;
+                Console.Write((char)t);
+            }
+            Console.ReadLine();
+           
+          
+               
+        }
+    }
+}
